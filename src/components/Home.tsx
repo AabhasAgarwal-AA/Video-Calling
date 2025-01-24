a8520c3947a960da7d39cb172dea185edc8bc2c4
AabhasAgarwal-AA <ag7152@srmist.edu.in>
2025-01-24 21:12:57 +0530

    hero section completed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,14 +4,16 @@ import { Footer } from "./Footer";
 import { Testimonial } from "./Testimonial";
 import { Faq } from "./Faq";
 import { PartnerLogo } from "./PartnerLogo";
+import { Hero } from "./Hero";
 
 
 export function Home(){
     return <div>
         <NavBar />
+        <Hero />
+        <PartnerLogo />
         <Testimonial />
         <Faq />
-        <PartnerLogo />
         <Footer />
     </div>
 }
